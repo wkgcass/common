05497fb97f7f8f7939767c737f1da6982cc46ce8
wkgcass <wkgcass@hotmail.com>
2015-07-25 19:32:11 +0800

    new algorithm

--- Enhanced/src/cass/enhanced/mongo/Transaction.java
+++ Enhanced/src/cass/enhanced/mongo/Transaction.java
@@ -21,15 +21,14 @@ public class Transaction implements Closeable {
 	private static final int INITIAL = 0;
 	private static final int COMMITTING = 1;
 	private static final int ROLLINGBACK = -1;
-	private static final String MAP_REF = "object-ref";
-	private static final String MAP_COLL = "collection-name";
 	static final String TX_MAP_COLL = "j-tx-map-coll";
 	static final String TX_REF = "j-transaction-ref";
 	private static final String TX_BKUP = "j-tx-backup";
 	private static final String TX_REQUIRE_ROLLBACK_ON_ERROR = "j-tx-req-rolbk-on-err";
 	private static final String TX_COLL_PREFIX = "j_transaction_";
+	private static final String TX_DELETE = "j-tx-del";
 	private static List<Integer> transactionId = new LinkedList<Integer>();
-	private static Map<Integer, List<Map<String, Object>>> txMap = new HashMap<Integer, List<Map<String, Object>>>();
+	private static Map<Integer, List<Object>> txMap = new HashMap<Integer, List<Object>>();
 
 	private static Integer findId() {
 		synchronized (transactionId) {
@@ -54,28 +53,22 @@ public class Transaction implements Closeable {
 	}
 
 	private boolean requireMapping(MongoCollection<Document> coll, Object ref_id) {
-		List<Map<String, Object>> txList;
+		List<Object> idList;
 		synchronized (txMap) {
 			if (txMap.containsKey(id)) {
-				txList = txMap.get(id);
+				idList = txMap.get(id);
 			} else {
-				txList = new ArrayList<Map<String, Object>>();
-				txMap.put(id, txList);
+				idList = new ArrayList<Object>();
+				txMap.put(id, idList);
 			}
 		}
-		Map<String, Object> tmpMap = null;
-		synchronized (txList) {
-			for (Map<String, Object> map : txList) {
-				if (map.get(MAP_REF).equals(ref_id)) {
+		synchronized (idList) {
+			for (Object id : idList) {
+				if (id.equals(ref_id)) {
 					return false;
 				}
 			}
-			tmpMap = new HashMap<String, Object>();
-			txList.add(tmpMap);
-		}
-		synchronized (tmpMap) {
-			tmpMap.put(MAP_REF, ref_id);
-			tmpMap.put(MAP_COLL, coll.getNamespace().getCollectionName());
+			idList.add(ref_id);
 		}
 		return true;
 	}
@@ -90,6 +83,7 @@ public class Transaction implements Closeable {
 		// set mapping properties
 		doc.put(TX_REF, _id);
 		doc.put(TX_MAP_COLL, collName);
+		doc.put(TX_BKUP, doc);
 		return true;
 	}
 
@@ -161,33 +155,33 @@ public class Transaction implements Closeable {
 	DeleteResult deleteOne(MongoCollection<Document> coll, Bson query) {
 		Document doc = coll.find(query).first();
 		if (null != doc) {
-			Object _id = doc.get("_id");
-			if (!requireMapping(coll, _id)) {
-				txColl.deleteOne(
-						new BasicDBObject(TX_REF, _id).append(TX_MAP_COLL, coll.getNamespace().getCollectionName()));
+			if (requireDocCopy(coll, doc)) {
+				doc.append(TX_DELETE, true);
+				txColl.insertOne(doc);
+				return new MannualDeleteResult(1L, true);
 			}
-			return new MannualDeleteResult(1l, true);
 		}
-		return new MannualDeleteResult(0l, true);
+		return new MannualDeleteResult(0L, true);
 	}
 
 	DeleteResult deleteMany(MongoCollection<Document> coll, Bson query) {
 		FindIterable<Document> res = coll.find(query);
 		Iterator<Document> it = res.iterator();
 		long count = 0;
+		List<Document> listToInsert = new ArrayList<Document>();
 		while (it.hasNext()) {
-			++count;
 			Document doc = it.next();
-			Object _id = doc.get("_id");
-			if (!requireMapping(coll, _id)) {
-				txColl.deleteOne(
-						new BasicDBObject(TX_REF, _id).append(TX_MAP_COLL, coll.getNamespace().getCollectionName()));
+			if (requireDocCopy(coll, doc)) {
+				doc.append(TX_DELETE, true);
+				listToInsert.add(doc);
+				++count;
 			}
 		}
+		txColl.insertMany(listToInsert);
 		return new MannualDeleteResult(count, true);
 	}
 
-	MongoCollection<Document> getTxCollection() {
+	public MongoCollection<Document> getTxCollection() {
 		return txColl;
 	}
 
@@ -205,67 +199,29 @@ public class Transaction implements Closeable {
 
 	public void commit() {
 		state = COMMITTING;
-		List<Map<String, Object>> txList = null;
-		synchronized (txMap) {
-			txList = txMap.get(id);
-		}
-		if (null != txList) {
-			synchronized (txList) {
-				FindIterable<Document> toCommit = txColl.find();
-				for (Document doc : toCommit) {
-					String collName = (String) doc.remove(TX_MAP_COLL);
-					MongoCollection<Document> coll = db.getCollection(collName);
-					if (doc.containsKey(TX_REF)) {
-						// update
-						Object _id = doc.remove(TX_REF);
-						Object tx_id = doc.put("_id", _id);
-						Bson query = new BasicDBObject("_id", _id);
-						Iterator<Document> it = coll.find(query).iterator();
-						if (it.hasNext()) {
-							// no other tx delete operation
-							Document target = it.next();
-							target.remove("_id");
-							txColl.updateOne(new BasicDBObject("_id", tx_id), new BasicDBObject("$set",
-									new BasicDBObject(TX_BKUP, target).append(TX_REQUIRE_ROLLBACK_ON_ERROR, true)));
-
-							// do update
-							coll.replaceOne(new BasicDBObject("_id", _id), doc);
-
-							// delete map
-							Iterator<Map<String, Object>> mapIt = txList.iterator();
-							while (mapIt.hasNext()) {
-								Map<String, Object> map = mapIt.next();
-								if (map.get(MAP_REF).equals(_id) && map.get(MAP_COLL).equals(collName)) {
-									mapIt.remove();
-									break;
-								}
-							}
-						}
-					} else {
-						// insert
-						Object tx_id = doc.remove("_id");
-						coll.insertOne(doc);
-						Object _id = doc.get("_id");
-						txColl.updateOne(new BasicDBObject("_id", tx_id), new BasicDBObject("$set",
-								new BasicDBObject(TX_REQUIRE_ROLLBACK_ON_ERROR, true).append(TX_REF, _id)));
-					}
-				}
-				for (Map<String, Object> map : txList) {
+		FindIterable<Document> toCommit = txColl.find();
+		for (Document doc : toCommit) {
+			String collName = (String) doc.remove(TX_MAP_COLL);
+			doc.remove(TX_BKUP);
+			MongoCollection<Document> coll = db.getCollection(collName);
+			if (doc.containsKey(TX_REF)) {
+				Object _id = doc.remove(TX_REF);
+				boolean isDelete = doc.getBoolean(TX_DELETE, false);
+				if (isDelete) {
 					// delete
-					Object _id = map.get(MAP_REF);
-					String collName = (String) map.get(MAP_COLL);
-					MongoCollection<Document> coll = db.getCollection(collName);
-					FindIterable<Document> res = coll.find(new BasicDBObject("_id", _id));
-					Iterator<Document> it = res.iterator();
-					if (it.hasNext()) {
-						Document retrievedDoc = it.next();
-						retrievedDoc.remove("_id");
-						Document insertedDoc = new Document(TX_REF, _id).append(TX_MAP_COLL, collName).append(TX_BKUP,
-								retrievedDoc.append(TX_REQUIRE_ROLLBACK_ON_ERROR, true));
-						txColl.insertOne(insertedDoc);
-						coll.deleteOne(new BasicDBObject("_id", _id));
-					}
+					coll.deleteOne(new BasicDBObject("_id", _id));
+				} else {
+					// update
+					// do update
+					coll.replaceOne(new BasicDBObject("_id", _id), doc);
 				}
+			} else {
+				// insert
+				Object tx_id = doc.remove("_id");
+				coll.insertOne(doc);
+				Object _id = doc.get("_id");
+				txColl.updateOne(new BasicDBObject("_id", tx_id), new BasicDBObject("$set",
+						new BasicDBObject(TX_REQUIRE_ROLLBACK_ON_ERROR, true).append(TX_REF, _id)));
 			}
 		}
 		clear();
